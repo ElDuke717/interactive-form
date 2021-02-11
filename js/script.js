1e1f8887f7f3aa5732a7c0251d34d895529ba4ad
Nick Huemmer <njhuemmer@gmail.com>
2021-02-10 16:43:40 -0800

    starting to build out variables and focus states in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Test');
+const nameInput = document.getElementById('name');
+nameInput.focus();
+const focusTest = () => nameInput.focus ? 'yay!' : 'nay!';
+console.log(focusTest());
+
+const jobSelect = document.getElementById('title');
+console.log(jobSelect);
+console.log(jobSelect.value);
+const otherJob = document.getElementById('other-job-role');
+console.log(otherJob);
+
+const jobHide = () => jobSelect.value !== 'other' ? otherJob.style.display = "hidden": otherJob.style.display = "show";
+console.log(jobHide());
