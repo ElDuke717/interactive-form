5fe67083cbdb96a893dc914087f187f5ca3dc39b
Nick Huemmer <njhuemmer@gmail.com>
2021-02-11 16:35:51 -0800

    built loop, added variables for t-shirt elements

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ const designSelect = document.getElementById('design');
 const colorSelect = document.getElementById('color');
 const colorOption = document.querySelectorAll('#color option');
 
-console.log(designSelect);
-console.log(colorSelect);
-console.log(colorOption);
+// console.log(designSelect);
+// console.log(colorSelect);
+// console.log(colorOption);
 
 //nameInput.focus() makes the Name input box highlighted when the user opens the page. 
 nameInput.focus();
@@ -19,7 +19,17 @@ otherJob.style.display = 'none';
 
 //conditional logic in this event listener makes the 
 jobSelect.addEventListener('change', e => {
-    jobSelect.value === 'other' ? otherJob.style.display = "block": otherJob.style.display = "none";
+    jobSelect.value === 'othe r' ? otherJob.style.display = "block": otherJob.style.display = "none";
 });
 
-colorSelect.disable = 'true';
+colorSelect.disabled = 'true';
+
+designSelect.addEventListener('change', e => {
+    colorSelect.disabled = 'false'
+    for (let i = 0; i < colorOption.length; i++) {
+        const eventValue = e.target.value;
+        console.log(eventValue);
+        colorOption[i].getAttribute('value');
+        console.log(colorOption[i].getAttribute('value'));
+    }
+});
