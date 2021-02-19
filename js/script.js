eaec7f794808fa79a6e46dd6f6c3a98e821b17d0
Nick Huemmer <njhuemmer@gmail.com>
2021-02-19 14:33:51 -0800

    fixed issue with verifying activity check input, it now works.  Base project complete, now ready for exceeds work

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@ let totalCost = 0;
 
 //Selects all checkboxes on the page - based on input type 
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
 //Selects all the checkboxes that have been checked
 const checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
 //Payment selection variables
@@ -115,95 +116,96 @@ payment.children[1].setAttribute('class', 'selected');
 
 /**Event listener for payment method select. */
 payment.addEventListener('change', e =>{
-            if (e.target.value === 'credit-card') {
-                credit.style.display = 'block';
-                bitCoin.style.display = 'none';
-                payPal.style.display = 'none'
-             } else if (e.target.value === 'paypal') {
-                payPal.style.display = 'block';
-                credit.style.display = 'none';
-                bitCoin.style.display = 'none';
-             } else if (e.target.value === 'bitcoin') {
-                bitCoin.style.display = 'block';
-                credit.style.display = 'none';
-                payPal.style.display = 'none'
-             }        
-    }); 
+    if (e.target.value === 'credit-card') {
+        credit.style.display = 'block';
+        bitCoin.style.display = 'none';
+        payPal.style.display = 'none'
+    } else if (e.target.value === 'paypal') {
+        payPal.style.display = 'block';
+        credit.style.display = 'none';
+        bitCoin.style.display = 'none';
+    } else if (e.target.value === 'bitcoin') {
+        bitCoin.style.display = 'block';
+        credit.style.display = 'none';
+        payPal.style.display = 'none'
+    }        
+}); 
 
-    /**wholeForm event listener validates names and e-mail addresses before the form can be submitted. */
-    wholeForm.addEventListener('submit', e => {
-        console.log('form submittal')
-        const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameInput.value);
-        if (nameIsValid) {
-            nameInput.parentElement.classList.add('valid');
-        }        
-        if (!nameIsValid) {
-            e.preventDefault();
-            alert('Please enter a valid name.');
-            nameInput.parentElement.classList.add('not-valid');
-            nameInput.parentElement.classList.remove('valid');
-            nameInput.parentElement.lastElementChild.style.display = 'block';
-        }
-        const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
-        if (emailIsValid) {
-            emailInput.parentElement.classList.add('valid');
-        }        
-        if (!emailIsValid) {
-            e.preventDefault();
-            alert('Please enter a valid e-mail address.');
-            emailInput.parentElement.classList.add('not-valid');
-            emailInput.parentElement.classList.remove('valid');
-            emailInput.parentElement.lastElementChild.style.display = 'block';
-        }
-        const creditValidation = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/.test(ccNumber.value);
-        //regex for cc number from w3resource https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-2.php
-        if (creditValidation) {
-            ccNumber.parentElement.classList.add('valid');
-        }
-        if (!creditValidation){
-            e.preventDefault();
-            alert('Please enter a valid credit card number');
-            ccNumber.parentElement.classList.add('not-valid');
-            ccNumber.parentElement.classList.remove('valid');
-            ccNumber.parentElement.lastElementChild.style.display = 'block';
-        }
-        const zipValidation = /^\d{5}(?:[-\s]\d{4})?$/.test(zipCode.value);
-        //zip code validator from stack overflow https://stackoverflow.com/questions/2577236/regex-for-zip-code
-        if (zipValidation) {
-            zipCode.parentElement.classList.add('valid');
-        }
-        if (!zipValidation) {
-            e.preventDefault();
-            alert('Please enter a valid zip code.');
-            zipCode.parentElement.classList.add('not-valid');
-            zipCode.parentElement.classList.remove('valid');
-            zipCode.parentElement.lastElementChild.style.display = 'block';
-
-        }
-        const cvvValidation = /^\d{3}$/.test(cvv.value);
-        if (cvvValidation) {
-            cvv.parentElement.classList.add('valid');
-        }
-        if (!cvvValidation) {
-            e.preventDefault();
-            alert('Please enter a valid CVV number.');
-            cvv.parentElement.classList.add('not-valid');
-            cvv.parentElement.classList.remove('valid');
-            cvv.parentElement.lastElementChild.style.display = 'block';
-        }
-        /**This section checks to ensure that at least one activity is checked */
-        let checks = [];
-        checkedBoxes.forEach((c) => {
-        checks.push(c.checked);
-        });
-        if (checks.length === 0) {
-            e.preventDefault();
-            alert('Please select at least one activity.')
-        }
-        //Remove this when validation is complete.
+/**wholeForm event listener validates names and e-mail addresses before the form can be submitted. */
+wholeForm.addEventListener('submit', e => {
+    console.log('form submittal')
+    const nameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameInput.value);
+    if (nameIsValid) {
+        nameInput.parentElement.classList.add('valid');
+    }        
+    if (!nameIsValid) {
         e.preventDefault();
-
-    });
+        alert('Please enter a valid name.');
+        nameInput.parentElement.classList.add('not-valid');
+        nameInput.parentElement.classList.remove('valid');
+        nameInput.parentElement.lastElementChild.style.display = 'block';
+    }
+    const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
+    if (emailIsValid) {
+        emailInput.parentElement.classList.add('valid');
+    }        
+    if (!emailIsValid) {
+        e.preventDefault();
+        alert('Please enter a valid e-mail address.');
+        emailInput.parentElement.classList.add('not-valid');
+        emailInput.parentElement.classList.remove('valid');
+        emailInput.parentElement.lastElementChild.style.display = 'block';
+    }
+    const creditValidation = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/.test(ccNumber.value);
+    //regex for cc number from w3resource https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-2.php
+    if (creditValidation) {
+        ccNumber.parentElement.classList.add('valid');
+    }
+    if (!creditValidation){
+        e.preventDefault();
+        alert('Please enter a valid credit card number');
+        ccNumber.parentElement.classList.add('not-valid');
+        ccNumber.parentElement.classList.remove('valid');
+        ccNumber.parentElement.lastElementChild.style.display = 'block';
+    }
+    const zipValidation = /^\d{5}(?:[-\s]\d{4})?$/.test(zipCode.value);
+    //zip code validator from stack overflow https://stackoverflow.com/questions/2577236/regex-for-zip-code
+    if (zipValidation) {
+        zipCode.parentElement.classList.add('valid');
+    }
+    if (!zipValidation) {
+        e.preventDefault();
+        alert('Please enter a valid zip code.');
+        zipCode.parentElement.classList.add('not-valid');
+        zipCode.parentElement.classList.remove('valid');
+        zipCode.parentElement.lastElementChild.style.display = 'block';
+    }
+    const cvvValidation = /^\d{3}$/.test(cvv.value);
+    if (cvvValidation) {
+        cvv.parentElement.classList.add('valid');
+    }
+    if (!cvvValidation) {
+        e.preventDefault();
+        alert('Please enter a valid CVV number.');
+        cvv.parentElement.classList.add('not-valid');
+        cvv.parentElement.classList.remove('valid');
+        cvv.parentElement.lastElementChild.style.display = 'block';
+    }
+    
+    /**This section checks to ensure that at least one activity is checked */   
+    const checkedCount = Array.prototype.slice.call(checkboxes).some(checks => checks.checked);    
+    if (checkedCount) {
+        activitiesBox.parentElement.classList.add('valid');
+    }
+    if (!checkedCount) {
+        activitiesBox.parentElement.classList.add('not-valid');
+        activitiesBox.parentElement.classList.remove('valid');
+        activitiesBox.parentElement.lastElementChild.style.display = 'block';   
+        alert('Please select at least one activity.')
+    }    
+    //Remove this when validation is complete.
+    e.preventDefault();
+});   
  
 /**for...of loops for focus and blur states on each activity.  I couldn't figure
  * out how to make this work using a conventional for loop and to make addEventListener
@@ -213,13 +215,11 @@ payment.addEventListener('change', e =>{
  registerActivity.addEventListener('focus', e => {
      console.log('Focusing on the inputs');
  })
-
     for (const checkbox of checkboxes) {
         checkbox.addEventListener('focus', e =>{
             e.target.parentNode.classList.add('focus');
             })
         };
-
     for (const checkbox of checkboxes) {
     checkbox.addEventListener('blur', e =>{
         e.target.parentNode.classList.remove('focus');
